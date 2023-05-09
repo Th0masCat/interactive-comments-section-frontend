58439f43fb44903bed91ee8cb1ba55b34c635fab
Th0masCat <catsaredogstoo@gmail.com>
2023-05-10 00:11:23 +0530

    fixed initial comment issue

--- src/components/ReplyBoxComponent.tsx
+++ src/components/ReplyBoxComponent.tsx
@@ -21,61 +21,61 @@ export default function ReplyBoxComponent(props: any) {
             username: user.name,
             post_content: comment,
             likes: 0,
-            parent_post: props.parent_id,
+            parent_post: props.parent_id ? props.parent_id : null,
         })
         mutate()
-        
+
         setComment('')
         console.log('You clicked submit.');
     }
 
-    
-        return (
-            <Card
-                w={props.w}
-                h={'auto'}
-                radius="lg" >
-                <Flex
-                    p={'sm'}
-                    gap="sm"
-                    justify={'space-between'}>
-                    <Avatar
-                        src={user.isLoggedin?endpoint + user.user_image: ''}
-                        alt="user_avatar"
-                        sx={{ borderRadius: '20rem' }}
-                    />
-                    <Textarea
-                        placeholder='Write a reply...'
-                        value={comment}
-                        onChange={(e) => setComment(e.target.value)}
-                        size='sm'
-                        autosize
-                        radius={'md'}
-                        w={'100%'}
-                        minRows={3}
-                    >
-                    </Textarea>
-                    <Button
-                        disabled={comment === ''}
-                        onClick={(e) => handleSubmit(e)}
-                        size='sm'
-                        styles={(theme) => ({
-                            root: {
-                                backgroundColor: theme.colors.sitePrimary[0],
-                                color: theme.colors.siteNeutral[4],
-                                '&:hover': {
-                                    backgroundColor: theme.colors.siteNeutral[3],
-                                    color: theme.colors.sitePrimary[0],
-                                }
-                            }
-                        })
 
+    return (
+        <Card
+            w={props.w}
+            h={'auto'}
+            radius="lg" >
+            <Flex
+                p={'sm'}
+                gap="sm"
+                justify={'space-between'}>
+                <Avatar
+                    src={user.isLoggedin ? endpoint + user.user_image : ''}
+                    alt="user_avatar"
+                    sx={{ borderRadius: '20rem' }}
+                />
+                <Textarea
+                    placeholder='Write a reply...'
+                    value={comment}
+                    onChange={(e) => setComment(e.target.value)}
+                    size='sm'
+                    autosize
+                    radius={'md'}
+                    w={'100%'}
+                    minRows={3}
+                >
+                </Textarea>
+                <Button
+                    disabled={comment === ''}
+                    onClick={(e) => handleSubmit(e)}
+                    size='sm'
+                    styles={(theme) => ({
+                        root: {
+                            backgroundColor: theme.colors.sitePrimary[0],
+                            color: theme.colors.siteNeutral[4],
+                            '&:hover': {
+                                backgroundColor: theme.colors.siteNeutral[3],
+                                color: theme.colors.sitePrimary[0],
+                            }
                         }
-                    >
-                        Reply
-                    </Button>
-                </Flex>
-            </Card>
-        );
-    
+                    })
+
+                    }
+                >
+                    Reply
+                </Button>
+            </Flex>
+        </Card>
+    );
+
 }
